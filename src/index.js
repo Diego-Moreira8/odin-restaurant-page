44ea8d3c1df7b477347bedf8e1ca97dbf76ed22c
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2024-01-24 15:07:19 -0300

    feat: header and footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import homeContent from "./pages/home.js";
 import menuContent from "./pages/menu.js";
 import aboutContent from "./pages/about.js";
+import "./styles.scss";
 
 const content = document.querySelector("#content");
-content.textContent = "Teste";
